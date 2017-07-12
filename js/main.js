67d29e713ee8e67d240bfb1f14c143f7d2faa1d6
Liana Penn <lianapenn@gmail.com>
2017-07-11 20:40:15 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
- console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+//console.log("Up and running!");
+//console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = ["queen"]
+//var cardOne = cards [0];
+cardsInPlay.push('cardOne');
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
-
+var cardTwo = ["king"];
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+  console.log ("Two cards in play")
+ if(cardsInPlay[0] === cardsInPlay [1]) {
+  alert ("You found a match!");
+}
+else {
+  alert ("Sorry, try again.")
+}
+}
